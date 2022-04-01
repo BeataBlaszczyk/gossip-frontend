63640d21b76cd06d797f2bca32b49c47e8e9db17
Blaszczyk <beata.blaszczyk@capgemini.com>
2022-04-01 09:54:05 +0200

    cookies ok- try t di submit - still without registration

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -91,6 +91,7 @@ function secretsy(){
    const expireD = (new Date(Date. now()+ 86400*1000)). toUTCString()
       console.log(encodeURIComponent(res.data));
       document.cookie="connect.sid="+res.data+"; expires="+expireD;
+      console.log("tuz przed navigate")
       navigate("/secrets")
       //console.log("tuz przed navigate")
       //console.log("?")
